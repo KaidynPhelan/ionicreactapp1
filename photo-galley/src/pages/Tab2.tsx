692eeb4a29073d9debb70ddcaf8154361072235a
Kaidyn <kaidyn.phelan08@gmail.com>
2022-04-08 02:01:37 +1000

    created first ionic app and built camera feature

---
+++ photo-galley/src/pages/Tab2.tsx
@@ -0,0 +1,42 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet,} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonFab vertical='bottom' horizontal='center' slot='fixed'>
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+        
+      </IonContent>
+      <IonContent>
+          <IonGrid>
+            <IonRow>
+              {photos.map((photo, index) => (
+                <IonCol size="3" key={index}>
+                  <IonImg src={photo.webviewPath} />
+                </IonCol>
+              ))}
+            </IonRow>
+          </IonGrid>
+
+      </IonContent>
+
+    </IonPage>
+  );
+};
+
+export default Tab2;
